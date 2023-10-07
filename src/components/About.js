7700af67b2ef9ca9049130cb0a0148e5a1343193
robzero <robertarm82@yahoo.com>
2023-10-07 12:24:48 -0400

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Robert
+            <br className="hidden lg:inline-block" />I'm the #1 Fiverr Map Developer
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            I've been creating Minecraft Maps for over 10 years, and since
+            2022 I've founded Robarm LLC, a company dedicated to providing
+            top of the line Minecraft services ranging from map-making to
+            plugin development and much more. Got an idea? So do I, let's
+            get started!
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./profile.jpg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const projects = [
+  {
+    title: "Sweet Robot Empire",
+    subtitle: "Sci-fi Minecraft Server",
+    description:
+      "One of the most ambitious Sci-Fi servers ever made! Want to see for yourself? Join now!",
+    image: "./sweetrobotempire.gif",
+    link: "https://www.planetminecraft.com/server/sweet-robot-empire/",
+  },
+  {
+    title: "Robarm's Ultimate Tree Repository",
+    subtitle: "Free Download!",
+    description:
+      "One of, if not the largest tree repository tool for Minecraft map-makers. Completely free to use!",
+    image: "./TreeGif.gif",
+    link: "https://builtbybit.com/resources/free-tree-repository-over-300-schems.31286/?preview=1",
+  },
+  {
+    title: "Fiverr",
+    subtitle: "Create something with me!",
+    description:
+      "Whether it's a plugin that needs to be coded, a map or build you need done, or even a model/animation, I'm here to help!",
+    image: "./fiverr.gif",
+    link: "https://www.fiverr.com/robarm99",
+  },
+  {
+    title: "Minecraft Plugins",
+    subtitle: "Coming soon!",
+    description:
+      "I've got a couple upcoming plugins that you can obtain, coming soon! Including Loot Chests, Keycards, and more!",
+    image: "./coding.gif",
+    link: "https://www.fiverr.com/robarm99/create-a-minecraft-plugin",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Robarm has been a most satisfactory experience. Initial concerns were quickly ironed out and any issues, or problems from there on out were quickly and accurately addressed. Rob has an awesome attitude towards customer service, and if you treat him with respect, he'll treat you that and double. His map making skills are very impressive and the end result is nothing less than gorgeous. High recommendations! Those 5-stars of 'is didn't come from nowhere.",
+    image: "./profilepic3.png",
+    name: "Reddoesstuff",
+    company: "Sweden",
+  },
+  {
+    quote:
+      "He put in an amazing amount of detail into the map. Even though our team was looking for some changes which included revamping a lot of the map, he did it all in an extremely short time without sacrificing quality at all. Throughout the process he was very quick to respond and very friendly and helpful. I can 100% recommend him as builder. If our team ever needs more maps, we will certainly contract him again!",
+    image: "./profilepic4.png",
+    name: "Ninthaquila",
+    company: "Switzerland",
+  },
+  {
+    quote:
+      "Robarm made a great model with animations and delivered it very fast. He was very considerate and communicated through the process. Will work with him again for sure.",
+    image: "./profilepic2.png",
+    name: "Matheus_serafim",
+    company: "Brazil",
+  },
+  {
+    quote:
+      "Great working with robarm99. I appreciate the excellent communication and attention to detail in the build. I'll definitely be working with them again in the future for other projects. Definitely recommend to anyone looking to get a whole map made.",
+    image: "./profilepic1.png",
+    name: "Patbutalsorick",
+    company: "United Kingdom",
+  }
+];
+
+export const skills = [
+  {
+    title: "Java Minecraft Plugins",
+    link: "https://www.fiverr.com/robarm99/create-a-minecraft-plugin"
+  },
+  {
+    title: "Terraforming",
+    link: "https://www.fiverr.com/robarm99/create-a-custom-minecraft-map"
+  },
+  {
+    title: "Building",
+    link: "https://www.fiverr.com/robarm99/build-a-minecraft-city-or-castle-for-you"
+  },
+  {
+    title: "Model & Animation",
+    link: "https://www.fiverr.com/robarm99/create-and-animate-minecraft-blockbench-model"
+  },
+  {
+    title: "Cinematography",
+    link: "https://www.fiverr.com/robarm99/create-a-minecraft-cinematic-for-you"
+  },
+  {
+    title: "Server Building / Configuration",
+    link: "https://www.fiverr.com/robarm99/create-a-minecraft-plugin"
+  }
+];
